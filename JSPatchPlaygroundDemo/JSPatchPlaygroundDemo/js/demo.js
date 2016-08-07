97d3bbc94825592bcee0903272ee685dd621b8b0
/ <65301524@qq.com>
2016-08-07 22:52:00 +0800

    readme

--- JSPatchPlaygroundDemo/JSPatchPlaygroundDemo/js/demo.js
+++ JSPatchPlaygroundDemo/JSPatchPlaygroundDemo/js/demo.js
@@ -6,10 +6,12 @@ var screenHeight = UIScreen.mainScreen().bounds().height;
 defineClass('JPDemoController: UIViewController', {
             viewDidLoad: function() {
             self.super().viewDidLoad();
-            self.view().setBackgroundColor(UIColor.redColor());
+            self.view().setBackgroundColor(UIColor.whiteColor());
             var size = 120;
             var imgView = UIImageView.alloc().initWithFrame({x: (screenWidth - size)/2, y: 150, width: size, height: size});
             imgView.setImage(UIImage.imageNamed('apple'))
+            
+            aa
 //            imgView.setImage(UIImage.imageWithContentsOfFile(resourcePath('apple.png')));
             self.view().addSubview(imgView);
 
